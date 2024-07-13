da7edaeb876014febbf9c5e8f20628e05f47fd7b
chaojie123 <1070718455@qq.com>
2024-07-13 23:17:56 +0800

    assds

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './CICD-DEMO/',
+  base: './zhangchaojie.github.io/CICD-DEMO/',
   plugins: [vue()],
 })
